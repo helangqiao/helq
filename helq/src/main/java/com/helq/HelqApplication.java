2d56c356c7fcee3332faee35d023068d22a83d12
helq <helq@dingchi.com>
2023-03-30 16:07:40 +0800

    first commit test

---
+++ helq/src/main/java/com/helq/HelqApplication.java
@@ -0,0 +1,13 @@
+package com.helq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ helq/src/test/java/com/helq/HelqApplicationTests.java
@@ -0,0 +1,3 @@
+package com.helq;
+
+class HelqApplicationTests {
